60b1cfc6ee232eaa97492e1a0cf55a4c5c7c86b9
Tim L <timl@Zipcoders-MacBook-Pro.local>
2024-07-05 11:02:33 -0400

    gIsHappy completed

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -18,8 +18,8 @@ public class StringsAndThings {
         int words = 0; // set word counter to 0
         for (int i = 0; i < input.length(); i++) { //for loop to loop through the string
             if (input.charAt(i) == 'y' || input.charAt(i) == 'z') { // searching for y and z in 'input'
-                if ((i == (input.length() - 1)) || !Character.isLetter(input.charAt(i + 1))) {
-                    words++;
+                if ((i == (input.length() - 1)) || !Character.isLetter(input.charAt(i + 1))) {// checks if the last letter is y or z, also checks if the space after the end of teh word is blank
+                    words++;//increments the count
                 }
             }
         }
@@ -35,7 +35,25 @@ public class StringsAndThings {
      *           removeString("Hello there", "x") // Should return "Hello there"
      */
     public String removeString(String base, String remove){
-        return null;
+        String input = base;
+        input = input.replace(remove, ""); // places 'remove' with empty ""
+        return input; // return input.replace(remove, ""); - condensed return statement
+
+        //Below works as well more complicated
+//        int find = 0; //stores the index of the substring 'remove' in the base string
+//        int previous = 0; //keeps track of the end of the last non-matching segment in base string
+//        StringBuilder result = new StringBuilder(base.length());//initialize stringbuilder named result equal to the length of the base string.
+//        // this will store the string after all 'remove's have been removed
+//        while ((find = base.indexOf(remove, previous)) >=0){ //searches from substring 'remove' in 'base' string from index 'previous
+//            //the result is assigned to find
+//            // continues as long as the substring was found
+//            result.append(base.substring(previous, find));//Extracts the substring from 'base' starting at 'previous' and ending just before 'find'.
+//            //Appends this non-matching segment to the result StringBuilder.
+//            previous = find + remove.length();//Updates previous to the index just after the end of the found substring remove
+//        }
+//        result.append(base.substring(previous));//extracts the substring  from 'base', starting at previous to th end of the string
+//        //& appends this remaining segment to the 'result' stringbuilder.
+//        return result.toString();
     }
 
     /**
@@ -47,7 +65,14 @@ public class StringsAndThings {
      *           containsEqualNumberOfIsAndNot("noisxxnotyynotxisi") // Should return true
      */
     public Boolean containsEqualNumberOfIsAndNot(String input){
-        return null;
+        String removeNot = input.replaceAll("not", "");//removes all 'not's from input
+        String removeIs = input.replaceAll("is", "");//removes all 'is'
+
+        int notCount = (input.length() - removeNot.length()) / 3; //calculates # of characters removed after 'not' was removed
+        int isCount = (input.length() - removeIs.length()) /2;
+        //Since each "not" substring has 3 characters, dividing by 3 gives the count of "not" substrings in the original input string
+
+        return isCount == notCount;
     }
 
     /**
@@ -57,9 +82,20 @@ public class StringsAndThings {
      *           gHappy("xxgxx") // Should return  false
      *           gHappy("xxggyygxx") // Should return  false
      */
-    public Boolean gIsHappy(String input){
-        return null;
+    public Boolean gIsHappy(String input) {
+        Boolean result = false;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == 'g') {
+                if (input.charAt(i - 1) == 'g' || input.charAt(i + 1) == 'g') {
+                    result = true;
+                } else {
+                    result = false;
+                }
+            }
+        }
+        return result;
     }
+
 
 
     /**
